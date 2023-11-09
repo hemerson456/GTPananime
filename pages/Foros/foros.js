ca26fc210204a50e5b7d62e9ea7ae7c698b62efc
hemerson456 <hngtraeders@gmail.com>
2023-11-08 23:19:50 -0500

    versiones122

--- pages/Foros/foros.js
+++ pages/Foros/foros.js
@@ -38,3 +38,52 @@ document.addEventListener("DOMContentLoaded", function () {
         interval = setInterval(nextSlide, 6000);//6000 es los milisegundos osea 6s equivale a 6000ms
     });
 });
+
+
+$(document).ready(function(){
+    
+    // Agregar clase active
+    $('.etiquetas_list .category_item[category="all"]').addClass('ct_item-active');
+
+    // Filtrar
+    $('.category_item').click(function(){
+        var catProduct = $(this).attr('category');
+        console.log(catProduct);
+
+        // Agregar clase active a la categoría seleccionada
+        $('.category_item').removeClass('ct_item-active');
+        $(this).addClass('ct_item-active');
+
+        // Ocultar productos
+        $('.anime-li').css('transform', 'scale(0)');
+        function hideProduct(){
+            $('.anime-li').hide();
+        }
+        setTimeout(hideProduct, 400);
+
+        // Mostrar productos correspondientes a las categorías seleccionadas
+        function showProduct(){
+            if (catProduct === 'all') {
+                $('.anime-li').show();
+                $('.anime-li').css('transform', 'scale(1)');
+            } else {
+                // Separa las categorías por comas y las convierte en un arreglo
+                var categories = catProduct.split(',');
+                for (var i = 0; i < categories.length; i++) {
+                    var category = categories[i].trim(); // Elimina espacios en blanco
+                    $('.anime-li[category*="'+category+'"]').show();
+                    $('.anime-li[category*="'+category+'"]').css('transform', 'scale(1)');
+                }
+            }
+        }
+        setTimeout(showProduct, 400);
+    });
+
+    // Hacer que la categoría "all" muestre todos los productos
+    $('.category_item[category="all"]').click(function(){
+        $('.anime-li').show();
+        $('.anime-li').css('transform', 'scale(1)');
+    });
+
+});
+  
