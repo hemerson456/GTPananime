ec27094a975393757c81f23d0bd28133fede60a3
hemerson456 <hngtraeders@gmail.com>
2023-11-18 19:23:00 -0500

    versiones161

--- loader.js
+++ loader.js
@@ -7,3 +7,33 @@ window.addEventListener("load", () => {
         document.body.removeChild("loader");
     })
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Tu código JavaScript aquí
+    const cursor = document.querySelector(".cursor");
+    var timeout;
+
+    document.addEventListener("mousemove", (e) => {
+        let x = e.pageX;
+        let y = e.pageY;
+
+        cursor.style.top = y + "px";
+        cursor.style.left = x + "px";
+        cursor.style.display = "block";
+
+        function mouseStopped() {
+            cursor.style.display = "none";
+        }
+
+        clearTimeout(timeout);
+        timeout = setTimeout(mouseStopped, 1000);
+    });
+
+    document.addEventListener("click", () => {
+        cursor.style.display = "none";
+    });
+
+    document.addEventListener("mouseout", () => {
+        cursor.style.display = "none";
+    });
+});
